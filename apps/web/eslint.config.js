8dd1873179cbd19caf10ca5f8b196554574290a4
Krunal Shah <krunal@infynno.com>
2026-03-12 15:19:57 +0530

    initial commit

---
+++ apps/web/eslint.config.js
@@ -0,0 +1,4 @@
+import { nextJsConfig } from "@ai-starter/eslint-config/next-js";
+
+/** @type {import("eslint").Linter.Config[]} */
+export default nextJsConfig;
---
+++ packages/ui/eslint.config.mjs
@@ -0,0 +1,4 @@
+import { config } from "@ai-starter/eslint-config/react-internal";
+
+/** @type {import("eslint").Linter.Config} */
+export default config;
