914447c0adad79e52601742fff7a83185e14ffc1
Krunal Shah <krunal@infynno.com>
2026-03-12 16:30:50 +0530

    chore: add Prettier configuration and update ESLint plugins for improved code formatting and linting

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -3,6 +3,9 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
+import prettierPlugin from "eslint-plugin-prettier";
+import unusedImportsPlugin from "eslint-plugin-unused-imports";
+import simpleImportSortPlugin from "eslint-plugin-simple-import-sort";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -24,6 +27,34 @@ export const config = [
   {
     plugins: {
       onlyWarn,
+    },
+  },
+  {
+    plugins: {
+      prettier: prettierPlugin,
+      "unused-imports": unusedImportsPlugin,
+      "simple-import-sort": simpleImportSortPlugin,
+    },
+    rules: {
+      // Prettier
+      "prettier/prettier": "error",
+
+      // Unused imports / vars
+      "no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+
+      // Import / export sorting
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
   },
   {
